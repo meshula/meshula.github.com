570742b68c991d6c9290906cdae626532d43926b
Nick Porcino <nick.porcino@localhost>
2025-08-24 19:06:04 -0700

    update config

--- site/astro-site/astro.config.mjs
+++ site/astro-site/astro.config.mjs
@@ -7,7 +7,7 @@ import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tritryam.org', // Updated for production
+	site: 'https://nickporcino.com', // Updated for production
 	integrations: [
 		mdx({
 			remarkPlugins: [remarkMath],
--- site/astro-site/src/consts.ts
+++ site/astro-site/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tritryam';
+export const SITE_TITLE = 'Nick Porcino';
 export const SITE_DESCRIPTION = 'Technology for Consciousness';
